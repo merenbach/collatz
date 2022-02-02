ce4e8ceca045485d28f72f9b22f918a64d4fea73
Andrew Merenbach <andrew@merenbach.com>
2022-02-02 00:39:50 -0800

    Generalize the Collatz problem

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,22 @@ mod tests {
     }
 }
 
-fn collatz(i: u64) -> Vec<u64> {
-    successors(Some(i), |&n| match n {
-        n if n == 1 => None,
-        n if n % 2 == 1 => Some(3 * n + 1),
-        _ => Some(n / 2),
+fn collatz_generalized(seed: i64, d: i64, m_0: i64, m_1: i64, r_0: i64, r_1: i64) -> Vec<i64> {
+    successors(Some(seed), |&n| match n {
+        n if n == m_0 => None,
+        n if n % d == 1 => Some(m_1 * n - r_1),
+        _ => Some((m_0 * n - r_0) / d), // TODO: is this correct placement for m_0 and r_0?
     })
     .collect()
 }
 
+fn collatz(seed: i64) -> Vec<i64> {
+    return collatz_generalized(seed, 2, 1, 3, 0, -1);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let seed = args[1].parse::<u64>().unwrap();
+    let seed = args[1].parse::<i64>().unwrap();
 
     for out in collatz(seed) {
         println!("{}", out);
